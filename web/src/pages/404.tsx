3855797c556029c6285d7ac09d14b9558d521652
Alana Bacco <alana_bacco@hotmail.com>
2023-05-09 17:21:41 -0300

    web: ajusta estilo da página 404

--- web/src/pages/404.tsx
+++ web/src/pages/404.tsx
@@ -1,13 +1,24 @@
-import Head from "@/utils/Head";
+"use-client";
+
+import Head from "src/utils/Head";
 import Link from "next/link";
+import style404 from "../styles/pagina404.module.css";
+import comumStyles from "../styles/comum.module.css";
+import Footer from "./components/Footer";
 
 export default function Pagina404() {
   return (
     <>
       <Head title="404 | Passeios Turísticos de Borborema" />
+      
+      <div className={comumStyles.mainContainer}>
+        <div className={style404.container}>
+          <h1>404 - A página que você procura não foi encontrada</h1>
+          <Link href="/">Voltar à Página Inicial</Link>
+        </div>
+      </div>
 
-      <h1>404 - A página que você procura não foi encontrada</h1>
-      <Link href="/">Voltar à Página Inicial</Link>
+      <Footer />
     </>
   );
 }
