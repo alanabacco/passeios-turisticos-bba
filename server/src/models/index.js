abaa9ab74fdc0486d5aa83d62b81476a711144bd
Alana Bacco <alana_bacco@hotmail.com>
2023-05-16 18:15:48 -0300

    server: require("pg")

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -9,6 +9,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
+require("pg");
 
 let sequelize;
 if (config.use_env_variable) {
