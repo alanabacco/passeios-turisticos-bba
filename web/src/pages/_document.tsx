0fbaf09c63f5748f3739c49e6afdfda064356991
Alana Bacco <alana_bacco@hotmail.com>
2024-03-16 15:34:10 -0300

    web: adiciona google analytics

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Analytics from "src/components/Analytics";
 import VLibrasWidget from "src/components/VLibrasWidget";
 
 export default function Document() {
@@ -16,6 +17,7 @@ export default function Document() {
         <Main />
         <NextScript />
         <VLibrasWidget />
+        <Analytics />
       </body>
     </Html>
   );
