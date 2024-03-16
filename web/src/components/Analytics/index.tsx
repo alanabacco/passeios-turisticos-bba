909b793f555871c96abb07a3dc6940994d57c2fe
Alana Bacco <alana_bacco@hotmail.com>
2024-03-16 15:52:11 -0300

    web: corrige erro na string google analytics

--- web/src/components/Analytics/index.tsx
+++ web/src/components/Analytics/index.tsx
@@ -13,7 +13,7 @@ const Analytics = () => (
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());        
-          gtag('config', ${process.env.NEXT_PUBLIC_GA_TRACKING_ID});
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
         `,
       }}
     />
